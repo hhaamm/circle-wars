3b7fa350497e63a1de989156909949d563cec553
hhaamm <ham1988@gmail.com>
2014-05-22 15:19:29 -0300

    Added menus

--- src/script.js
+++ src/script.js
@@ -16,9 +16,28 @@ debug = function(message) {
 		window.console.log(message);
 };
 
-$(document).ready(function() {	
-	var ctx = $('#canvas')[0].getContext("2d");		 
-	var game = new Game(ctx, $('#canvas').attr("width"), $('#canvas').attr("height"));
-	
-	game.init();
+$(document).ready(function() {
+    $("#newGameBtn").click(function() {
+        setStage("CreateGame");
+        return false;
+    });
+
+    $("#startGameBtn").click(function() {
+        setStage("Game");
+
+	    var ctx = $('#canvas')[0].getContext("2d");		 
+	    var game = new Game(ctx, $('#canvas').attr("width"), $('#canvas').attr("height"));
+	    
+	    game.init();        
+        
+        return false;
+    });
+    
+    setStage("Init");
 });
+
+setStage = function(stage) {
+    $(".stage").hide();
+    $("#stage"+stage).show();
+};
+
